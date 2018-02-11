dba15733e982c33d5c019de5e6a17007579e5987
Miguel Fontes <miguel_m_fontes@hotmail.com>
2018-02-11 16:28:00 -0200

    Refactor em lógica de validação de posições em Zona

--- robots-core/src/test/java/br/com/miguelmf/robots/core/ZoneTest.java
+++ robots-core/src/test/java/br/com/miguelmf/robots/core/ZoneTest.java
@@ -5,7 +5,9 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
 
+import static br.com.miguelmf.robots.core.Direction.WEST;
 import static br.com.miguelmf.robots.core.utils.TestUtils.moveNTimes;
+import static br.com.miguelmf.robots.core.utils.TestUtils.newRobotAtZeroPositionFacing;
 import static br.com.miguelmf.robots.core.utils.TestUtils.newRobotAtZeroPositionFacingNorth;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -31,7 +33,7 @@ class ZoneTest {
     }
 
     @Test
-    @DisplayName("should create a 5x5 zone with a robot at 0,0 position, facing NORTH")
+    @DisplayName("should update a Robot position")
     void shouldUpdateRobotPosition() throws IllegalPositionException {
         Position updatedPosition = new Position(0, 2);
         Zone zone = new Zone(5, 5);
@@ -56,14 +58,16 @@ class ZoneTest {
     @Test
     @DisplayName("should throw an IllegalPositionException when a given Robot has an position out of the Dimension bounds")
     void errorOnInvalidPostion() throws IllegalPositionException {
-        Position invalidPosition1= new Position(0,5);
-        Position invalidPosition2= new Position(0,6);
-        Dimension dimension = new Dimension(5,5);
+        Position invalidPosition1 = new Position(0, 5);
+        Position invalidPosition2 = new Position(0, 6);
+        Position invalidPosition3 = new Position(-1, 0);
+        Dimension dimension = new Dimension(5, 5);
         Zone zone = new Zone(dimension);
 
         Robot validRobot = moveNTimes(4, newRobotAtZeroPositionFacingNorth());
         Robot limitCaseInvalidRobot = moveNTimes(5, newRobotAtZeroPositionFacingNorth());
         Robot invalidRobot = moveNTimes(6, newRobotAtZeroPositionFacingNorth());
+        Robot invalidRobot2 = moveNTimes(1, newRobotAtZeroPositionFacing(WEST));
 
         zone.addRobot(validRobot);
 
@@ -73,13 +77,19 @@ class ZoneTest {
         Throwable exception2 = assertThrows(IllegalPositionException.class,
                 () -> zone.addRobot(invalidRobot));
 
+        Throwable exception3 = assertThrows(IllegalPositionException.class,
+                () -> zone.addRobot(invalidRobot2));
+
 
         assertAll(
                 () -> assertTrue(exception.getMessage().startsWith(String.format("Robot's position [%s] is out of the Zone Bounds [%s]",
                         invalidPosition1.toString(), dimension.toString()))),
 
                 () -> assertTrue(exception2.getMessage().startsWith(String.format("Robot's position [%s] is out of the Zone Bounds [%s]",
-                        invalidPosition2.toString(), dimension.toString())))
+                        invalidPosition2.toString(), dimension.toString()))),
+
+                () -> assertTrue(exception3.getMessage().startsWith(String.format("Robot's position [%s] is out of the Zone Bounds [%s]",
+                        invalidPosition3.toString(), dimension.toString())))
         );
 
     }
