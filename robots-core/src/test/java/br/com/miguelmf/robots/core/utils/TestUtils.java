4e2694e5131b7ee5536fec5563811cb1df5a9b0c
Miguel Fontes <miguel_m_fontes@hotmail.com>
2018-02-13 21:44:40 -0200

    Remoção de id de Robot

--- robots-core/src/test/java/br/com/miguelmf/robots/core/utils/TestUtils.java
+++ robots-core/src/test/java/br/com/miguelmf/robots/core/utils/TestUtils.java
@@ -7,11 +7,11 @@ import br.com.miguelmf.robots.core.Robot;
 public class TestUtils {
 
     public static Robot newRobotAtZeroPositionFacingNorth() {
-        return new Robot(0, Direction.NORTH, new Position(0, 0));
+        return new Robot(Direction.NORTH, new Position(0, 0));
     }
 
     public static Robot newRobotAtZeroPositionFacing(Direction direction) {
-        return new Robot(0, direction, new Position(0, 0));
+        return new Robot(direction, new Position(0, 0));
     }
 
     public static Robot moveNTimes(int times, Robot robot) {
